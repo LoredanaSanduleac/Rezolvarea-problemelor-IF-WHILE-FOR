80ef4e65bff87cf21f5cf2d730eabe1f51f44fac
Loredana Sanduleac <70658391+LoredanaSanduleac@users.noreply.github.com>
2020-11-10 10:04:39 +0300

    Add files via upload

---
+++ problema1LS.py
@@ -0,0 +1,9 @@
+n=eval(input("introduceti nr de zile: "))
+if n==28:
+    print("februarie")
+if n==29:
+    print("februarie(an bisect)")
+if n==30:
+    print("noiembrie, aprilie, iunie, septembrie")
+if n==31:
+    print("iulie, august, octombrie, decembrie, iaanuarie, martie, mai")
